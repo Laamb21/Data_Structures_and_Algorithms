80956cef2a470fe28c837dfecbb13ef79e72a1bb
Liam Slade <wslade1@students.towson.edu>
2024-07-22 16:58:09 -0400

    linear search python

---
+++ Python/Arrays/linear_search.py
@@ -0,0 +1,27 @@
+'''
+How Linear Search works:
+    1. Iterate through array
+    2. Compare each value to check if it is equal to the desired value
+    3. If value is found, return index of value
+    4. If value is not found by the end of the array, return -1 to indicate value was not found
+
+
+Time Complexity:
+Space Complexity: 
+'''
+
+#Linear Search function
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+#Example:
+arr = [1, 5 , 32, 56, 99, 69, 6, 50, 0, 45, 78, 8, 2, 4]
+target = 32
+result = linear_search(arr, target)
+if result == -1:
+    print("Value", target,"not found")
+else:
+    print("Value",target,"found at",result)
