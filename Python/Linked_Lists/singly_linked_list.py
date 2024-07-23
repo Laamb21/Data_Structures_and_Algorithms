f8cd35b18d7268d3e8edca5a05085d1812890c2e
Liam Slade <wslade1@students.towson.edu>
2024-07-23 13:32:16 -0400

    linked list operations

--- Python/Linked_Lists/singly_linked_list.py
+++ Python/Linked_Lists/singly_linked_list.py
@@ -8,6 +8,49 @@ class Node:
         self.data = data
         self.next = None
 
+#Function to find lowest value in a linked list
+def find_lowest_value(node):
+    min_value = node.data
+    currNode = node.next
+    while currNode:
+        if currNode.data < min_value:
+            min_value = currNode.data
+        currNode = currNode.next
+    return min_value
+
+
+#Function to delete a node from a linked list
+def delete_node(head, node_to_delete):
+    if head == node_to_delete:
+        return head.next
+    
+    currNode = head
+    while currNode.next and currNode.next != node_to_delete:
+        currNode = currNode.next
+    
+    if currNode.next is None:
+        return head
+    
+    currNode.next = currNode.next.next
+
+    return head
+
+#Function to insert a node in a linked list
+def insert_node(head, new_node, position):
+    if position == 1:
+        new_node.next = head
+        return new_node
+    
+    currNode = head
+    for _ in range(position - 2):
+        if currNode is None:
+            break
+        currNode = currNode.next
+
+    new_node.next = currNode.next
+    currNode.next = new_node
+    return head
+
 #Initialize Nodes with random integers
 node1 = Node(random.randint(0,100))
 node2 = Node(random.randint(0,100))
@@ -25,6 +68,28 @@ node4.next = node5
 currentNode = node1
 
 #Print Linked List
+while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+print("null")
+
+#Print lowest value 
+print("\nLowest value in linked list:",find_lowest_value(node1))
+
+#Delete a node
+node1 = delete_node(node1, node3)
+print("\nList after deleting Node 3:")
+currentNode = node1
+while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+print("null")
+
+#Replace deleted node with a new node 
+newNode = Node(random.randint(0,100))
+node1 = insert_node(node1, newNode, 3)
+print("\nList after inserting new node at position 3:")
+currentNode = node1
 while currentNode:
     print(currentNode.data, end=" -> ")
     currentNode = currentNode.next
